e83d8e433561cb64583c3b05fc186d22394f8002
Collin <collin@Collins-MacBook-Pro.local>
2017-12-06 12:01:27 +0800

    fork fork

---
+++ src/main/java/org/github/ddcfv/Main.java
@@ -0,0 +1,11 @@
+package org.github.ddcfv;
+
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(add(1,2));
+    }
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/org/github/ddcfv/MainTest.java
@@ -0,0 +1,12 @@
+package org.github.ddcfv;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void add() {
+    }
+}
